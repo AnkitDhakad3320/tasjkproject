c3bbbe8248d018bd005b184155264f85363d50c3
Ankit Dhakad <149234171+AnkitDhakad3320@users.noreply.github.com>
2025-07-31 11:20:26 +0530

    Update api.js
    api url

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5000/api/jobs";
+const BASE_URL = `${process.env.REACT_APP_API_URL}/api/jobs`;
 
 export const getJobs = () => axios.get(BASE_URL);
 export const getJobById = (id) => axios.get(`${BASE_URL}/${id}`);
